0b1c25811afe8fd22a6a22234423c606acf1f41c
Vinay <vinaysankar@gmail.com>
2020-05-27 04:17:27 +0530

    Third Commit

--- wcmedia/wcform/forms.py
+++ wcmedia/wcform/forms.py
@@ -9,4 +9,21 @@ class UserInfoForm(forms.ModelForm):
     class Meta:
         model = UserInfo
         fields = ['name', 'focus', 'market', 'requirement', 'domain', 'sales', 'expectation', 'audience', 'audi_you', 'market_status', 'hashtag', 'advt', 'company_profile',  'creation', 'expectation']
-        
+        widgets = { 'name': forms.TextInput(attrs={'size': 50})
+                    , 'focus': forms.TextInput(attrs={'size': 50})
+                    , 'market': forms.Textarea()
+                    , 'requirement': forms.Textarea() 
+
+                    , 'domain': forms.TextInput(attrs={'size': 100})
+                    , 'sales': forms.Textarea()
+                    , 'expectation': forms.Textarea() 
+
+                    , 'audience': forms.TextInput(attrs={'size': 100})
+                    , 'audi_you': forms.TextInput(attrs={'size': 100})
+                    , 'market_status': forms.Textarea()
+                    , 'hashtag': forms.TextInput(attrs={'size': 100})
+                    , 'advt': forms.Textarea()
+                    , 'company_profile': forms.TextInput(attrs={'size': 100})
+                    , 'creation': forms.TextInput(attrs={'size': 100})
+                    , 'expectation': forms.TextInput(attrs={'size': 20})
+                 }
